23b45a87f6c13ac05b648465bf5b6d72b496be0a
Foulques <f.demoncade@gmail.com>
2018-05-21 23:49:18 +0200

    failed

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,19 +1,72 @@
 import React from "react";
-import {ColokList} from './Colok.js'
-
+import { ColokList } from "./Colok.js";
+import Input from "react-toolbox/lib/input/Input";
 
 class Payment extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value : 0,
+			sharers : [],
+			name : '',
+		};
+
+		this.handleChange.bind(this);
+	}
+
+	handleChange() {
+		this.setState
+	}
+
 	render() {
 		return (
-
 			<div>
 
-			<h3> Avec qui avez-vous partagé ce paiement ? </h3>
-			<h4> Séléctionnez tous les bénéficiaires (dont vous) </h4>
+				<PaymentField value={this.state.value} onChange={this.handleChange('value')}/>
+				<p> Avec qui avez-vous partagé ce paiement ? </p>
+				<p> Séléctionnez tous les bénéficiaires (dont vous) </p>
 
-			<ColokList checkboxes />
+				<ColokList checkboxes onChange={this.handleChange('sharers')} />
 			</div>
+		);
+	}
+}
 
+class PaymentField extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			value: "",
+			error:"",
+		};
+
+		console.log(props);
+
+		this.handleChangeProps = this.props.onChange;
+
+		console.log(this.handleChangeProps);
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange() {
+		this.handleChangeProps(this.value);
+		this.setState({value : this.value});
+	}
+
+	render() {
+		return (
+			<Input
+				type="number"
+				label="How much did you spend ?"
+				name="value"
+				icon=" "
+				hint=""
+				error={this.state.error}
+				value={this.state.value}
+				onChange={this.handleChange}
+				maxLength={16}
+			/>
 		);
 	}
 }
